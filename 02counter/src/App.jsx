849ad5772cd7f51da7435d4f9f0a98a1da45e810
Kalyan0101 <naskarkalyan2000@outlook.com>
2024-09-10 20:39:56 +0530

    props

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,42 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
-  let [counter, setCounter] = useState(0)
-  
-  // let counter = 15;
+  const [counter, setCounter] = useState(0);
 
   const addValue = () => {
     // counter = counter + 1
-    if(counter < 20)
-      setCounter(counter + 1)
-    
-  }
-  const remValue = () => {
-    if(counter > 0){
-      setCounter(counter - 1)
+    if (counter < 20) {
+      setCounter(counter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter(prevCounter => prevCounter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+
     }
-  }
+  };
+  const remValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
-
-
     <>
       <h1>Chai aur React</h1>
       <h2>Counter Value: {counter}</h2>
-      <button
-        onClick={addValue}
-       >Add Value {counter} </button>
+      <button onClick={addValue}>Add Value {counter} </button>
       <br />
-      <button 
-        onClick={remValue}
-      >Remove Value {counter}</button>
+      <button onClick={remValue}>Remove Value {counter}</button>
       <p>{counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
